3222b4f3bebee111b24af1eb6692dd45fff78c22
Shalin Patel <shalin.kp.patel@gmail.com>
2021-01-23 15:55:34 -0500

    generative example done

--- src/NeuralGRN.jl
+++ src/NeuralGRN.jl
@@ -1,13 +1,14 @@
 module NeuralGRN
 
-using DiffEqFlux, DifferentialEquations, Flux, LightGraphs, SimpleWeightedGraphs, Plots, StatsPlots, GraphRecipes, CUDA
+using DiffEqFlux, DifferentialEquations, Flux, LightGraphs, SimpleWeightedGraphs,
+    Plots, StatsPlots, GraphRecipes, CUDA
 using BSON: @save, @load
 
 include("problem.jl")
 include("models.jl")
-include("generation/sample_problem.jl")
 
-export TemporalGRNProblem, SteadyStateGRNProblem
-export plot_problem_graph
+export TemporalGRNProblem, SteadyStateGRNProblem, GenerativePropODE,
+    ModelPropODE, DeepPropODE
+export plot_problem_graph, generate
 
 end
--- src/models.jl
+++ src/models.jl
@@ -1,26 +1,44 @@
 
 abstract type PropODE end
 
-struct GenerativePropODE{T <: Real, R <: Real, S <: Real} <: PropODE
+struct GenerativePropODE{T <: Real, R <: Real, S <: Real, M <: Function} <: PropODE
     x₀ :: Vector{T}
-    F :: Vector{Function}
+    F :: Vector{M}
     p :: Matrix{R}
+    g :: SimpleGraph
     τ :: Vector{S}
 end
 
 abstract type InferablePropODE <: PropODE end
 
-struct ModelPropODE{T <: Real, R <: Real} <: InferablePropODE
+struct ModelPropODE{T <: Real, R <: Real, S <: Real, M <: Function} <: InferablePropODE
     x₀ :: Vector{T}
-    F :: Vector{Function}
-    p :: Vector{R}
+    F :: Vector{M}
+    p :: Matrix{R}
     prob :: GRNProblem
+    τ :: Vector{S}
 end
 
-struct DeepPropODE{T <: Real} <: InferablePropODE
+struct DeepPropODE{T <: Real, R <: Real, S <: Real} <: InferablePropODE
     x₀ :: Vector{T}
     F :: Vector{FastChain}
+    p :: Matrix{R}
     prob :: GRNProblem
+    τ :: Vector{S}
 end
 
-function generate()
+get_graph(prop :: GenerativePropODE) = prop.g
+get_graph(prop :: T) where {T <: InferablePropODE} = prop.prob.g
+
+function generate(g :: PropODE)
+    function dynamic_system!(du, u, p, t)
+        for (i, f) ∈ enumerate(g.F)
+            n = neighbors(get_graph(g), i)
+            du[i] = f(u[n], p[i, n], t)
+        end
+    end
+
+    tspan = (g.τ[1], g.τ[end])
+    prob = ODEProblem(dynamic_system!, g.x₀, tspan, g.p, saveat = g.τ)
+    return solve(prob)
+end
